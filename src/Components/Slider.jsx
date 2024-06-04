f56b963c9f96aebd6b9fd2a111bc781ec9541d43
Purshottam Patidar <purshottam.patidar.28.1@mountblue.tech>
2024-06-04 11:16:52 +0530

    change in the slider is made

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Slider = () => {
   return (
     <Box minW={"10vw"} h={"100vh"} bg={"gray.400"}>
-    <Link to='/'>
+    <Link to='/boards'>
       <Button w={"full"}  h={'5vh'}>
         Boards
       </Button>
